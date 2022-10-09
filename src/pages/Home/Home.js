3817c60c15777d5c3b752f9bb669647f8b8e1b1b
BuiKiet <roiyano1utl@gmail.com>
2022-10-10 00:28:19 +0700

    13. Products tập 1

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,76 @@
 import classNames from 'classnames/bind';
 import styles from './Home.module.scss';
+import ProductItem from './ProductItem';
 
 const cx = classNames.bind(styles);
 
+let products = [
+    {
+        name: 'Ban Phim',
+        menu: [
+            {
+                name: 'Bam phim pro 12 nut bam!',
+                price: '500k',
+                img: require('~/assets/products/imgs/test.jpg'),
+            },
+            {
+                name: 'Bam phim test 2',
+                price: '600k',
+                img: require('~/assets/products/imgs/test.jpg'),
+            },
+        ],
+    },
+    {
+        name: 'Giay',
+        menu: [
+            {
+                name: 'Bana So nict',
+                price: '200k',
+                img: require('~/assets/products/imgs/test.jpg'),
+            },
+        ],
+    },
+];
+
 function Home() {
-    return <h1 className={cx('home')}>Home</h1>;
+    return (
+        <div className={cx('home')}>
+            <ProductItem
+                name="Phim coc oc oco cococo  cococo  co  "
+                source={require('~/assets/products/imgs/test.jpg')}
+                price="200k"
+            />
+            <ProductItem name="Phim co" source={require('~/assets/products/imgs/s1.jpg')} price="200k" />
+            <ProductItem name="Phim co" source={require('~/assets/products/imgs/test2.webp')} price="200k" />
+            <ProductItem name="Phim co" source={require('~/assets/products/imgs/test2.webp')} price="200k" />
+            <ProductItem name="Phim co" source={require('~/assets/products/imgs/test5.webp')} price="200k" />
+            <ProductItem uctItem name="Phim co" source={require('~/assets/products/imgs/s1.jpg')} price="200k" />
+            <ProductItem name="Phim co" source={require('~/assets/products/imgs/test2.webp')} price="200k" />
+            <ProductItem name="Phim co" source={require('~/assets/products/imgs/test2.webp')} price="200k" />
+            <ProductItem name="Phim co" source={require('~/assets/products/imgs/test5.webp')} price="200k" />
+            <ProductItem uctItem name="Phim co" source={require('~/assets/products/imgs/s1.jpg')} price="200k" />
+            <ProductItem name="Phim co" source={require('~/assets/products/imgs/test2.webp')} price="200k" />
+            <ProductItem name="Phim co" source={require('~/assets/products/imgs/test2.webp')} price="200k" />
+            <ProductItem name="Phim co" source={require('~/assets/products/imgs/test5.webp')} price="200k" />
+            <ProductItem uctItem name="Phim co" source={require('~/assets/products/imgs/s1.jpg')} price="200k" />
+            <ProductItem name="Phim co" source={require('~/assets/products/imgs/test2.webp')} price="200k" />
+            <ProductItem name="Phim co" source={require('~/assets/products/imgs/test2.webp')} price="200k" />
+            <ProductItem name="Phim co" source={require('~/assets/products/imgs/test5.webp')} price="200k" />
+            <ProductItem uctItem name="Phim co" source={require('~/assets/products/imgs/s1.jpg')} price="200k" />
+            <ProductItem name="Phim co" source={require('~/assets/products/imgs/test2.webp')} price="200k" />
+            <ProductItem name="Phim co" source={require('~/assets/products/imgs/test2.webp')} price="200k" />
+            <ProductItem name="Phim co" source={require('~/assets/products/imgs/test5.webp')} price="200k" />
+            <ProductItem uctItem name="Phim co" source={require('~/assets/products/imgs/s1.jpg')} price="200k" />
+            <ProductItem name="Phim co" source={require('~/assets/products/imgs/test2.webp')} price="200k" />
+            <ProductItem name="Phim co" source={require('~/assets/products/imgs/test2.webp')} price="200k" />
+            <ProductItem name="Phim co" source={require('~/assets/products/imgs/test5.webp')} price="200k" />
+            <ProductItem uctItem name="Phim co" source={require('~/assets/products/imgs/s1.jpg')} price="200k" />
+            <ProductItem name="Phim co" source={require('~/assets/products/imgs/test2.webp')} price="200k" />
+            <ProductItem name="Phim co" source={require('~/assets/products/imgs/test2.webp')} price="200k" />
+            <ProductItem name="Phim co" source={require('~/assets/products/imgs/test5.webp')} price="200k" />
+            <ProductItem name="Phim co" source={require('~/assets/products/imgs/test2.webp')} price="200k" />
+        </div>
+    );
 }
 
 export default Home;
---
+++ src/pages/Home/ProductItem/ProductItem.js
@@ -0,0 +1,27 @@
+import classNames from 'classnames/bind';
+import styles from '../Home.module.scss';
+
+const cx = classNames.bind(styles);
+
+function ProductItem({ name, source, price }) {
+    return (
+        <div className={cx('wapper')}>
+            <div className={cx('products')}>
+                <div className={cx('item')}>
+                    <div className={cx('wrap_img')}>
+                        <img className={cx('img')} src={source} />
+                    </div>
+                    <div className={cx('wrap__item-content')}>
+                        <h3 className={cx('name')}>{name}</h3>
+                        <p className={cx('wrap_price')}>
+                            <span className={cx('count_buy')}>Đã bán 24.5k</span>
+                            <i className={cx('price')}>{price}</i>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ProductItem;
